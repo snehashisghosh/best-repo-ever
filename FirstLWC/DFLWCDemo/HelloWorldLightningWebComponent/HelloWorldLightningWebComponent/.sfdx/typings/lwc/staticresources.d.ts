7e65908ea8a55eeb183052aae427bfcd26195c61
Ghosh <180582@cognizant.com>
2019-12-05 20:41:23 -0800

    LWC Recipe commit

---
+++ FirstLWC/DFLWCDemo/HelloWorldLightningWebComponent/HelloWorldLightningWebComponent/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,24 @@
+declare module "@salesforce/resourceUrl/chart" {
+    var chart: string;
+    export default chart;
+}
+declare module "@salesforce/resourceUrl/d3" {
+    var d3: string;
+    export default d3;
+}
+declare module "@salesforce/resourceUrl/moment" {
+    var moment: string;
+    export default moment;
+}
+declare module "@salesforce/resourceUrl/sampleExternalResource" {
+    var sampleExternalResource: string;
+    export default sampleExternalResource;
+}
+declare module "@salesforce/resourceUrl/trailhead_characters" {
+    var trailhead_characters: string;
+    export default trailhead_characters;
+}
+declare module "@salesforce/resourceUrl/trailhead_logo" {
+    var trailhead_logo: string;
+    export default trailhead_logo;
+}
